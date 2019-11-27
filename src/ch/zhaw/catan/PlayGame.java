e822dae3aa1eb54c3b66795017184b4bea672718
erman <zankoerm@students.zhaw.ch>
2019-11-27 10:13:07 +0100

    updated player creation method

--- src/ch/zhaw/catan/PlayGame.java
+++ src/ch/zhaw/catan/PlayGame.java
@@ -1,4 +1,5 @@
 package ch.zhaw.catan;
+import java.util.ArrayList;
 import java.util.Scanner;
 public class PlayGame {
     Scanner scanner = new Scanner(System.in);
@@ -7,6 +8,7 @@ public class PlayGame {
     private Player playerGreen;
     private Player playerRed;
     private Player playerYellow;
+    private ArrayList<Player> players;
 
     public int numberOfPlayers(){
         System.out.println("How many players will be playing?");
@@ -18,21 +20,34 @@ public class PlayGame {
         if(number == 2){
             playerBlue = new Player(Config.Faction.BLUE);
             playerGreen = new Player(Config.Faction.GREEN);
+            players = new ArrayList<>();
+            players.add(playerBlue);
+            players.add(playerGreen);
         } else if(number == 3){
             playerBlue = new Player(Config.Faction.BLUE);
             playerGreen = new Player(Config.Faction.GREEN);
             playerRed = new Player(Config.Faction.RED);
+            players = new ArrayList<>();
+            players.add(playerBlue);
+            players.add(playerGreen);
+            players.add(playerRed);
         } else if(number == 4){
             playerBlue = new Player(Config.Faction.BLUE);
             playerGreen = new Player(Config.Faction.GREEN);
             playerRed = new Player(Config.Faction.RED);
             playerYellow = new Player(Config.Faction.YELLOW);
+            players = new ArrayList<>();
+            players.add(playerBlue);
+            players.add(playerGreen);
+            players.add(playerRed);
+            players.add(playerYellow);
         } else{
             System.out.println("Error only between 2 and 4 players are allowed");
         }
     }
 
     public void firstPhase(){
+        createPlayers();
 
     }
 
