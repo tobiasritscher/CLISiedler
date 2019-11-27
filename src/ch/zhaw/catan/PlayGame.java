18939233c1a90054e8ce7bd17c442974c7f23b60
erman <zankoerm@students.zhaw.ch>
2019-11-27 16:11:36 +0100

    expanded firstPhase method

--- src/ch/zhaw/catan/PlayGame.java
+++ src/ch/zhaw/catan/PlayGame.java
@@ -1,5 +1,6 @@
 package ch.zhaw.catan;
 
+import ch.zhaw.hexboard.HexBoard;
 import org.beryx.textio.TextIO;
 import org.beryx.textio.TextIoFactory;
 import org.beryx.textio.TextTerminal;
@@ -11,10 +12,12 @@ import java.util.Scanner;
 
 
 public class PlayGame {
+    HexBoard hexBoard = new HexBoard();
     SiedlerGame siedlerGame = new SiedlerGame(7,numberOfPlayers());
     private Scanner scanner;
     Config config = new Config();
-
+    private Config.Faction faction;
+    Player player = new Player(faction);
 
     public enum Actions {
         NEW_GAME, QUIT
@@ -52,10 +55,25 @@ public class PlayGame {
 
     public void firstPhase(){
         for(int i = 0; i < siedlerGame.getPlayer().size(); i++){
-            System.out.println(siedlerGame.getPlayer().get(i) + "please pick a x and y coordinate for your first settlement");
+            System.out.println(siedlerGame.getPlayer().get(i) + "please pick a x coordinate for your first settlement");
             Scanner x = new Scanner(System.in);
+            System.out.println(siedlerGame.getPlayer().get(i) + "please pick a y coordinate for your first settlement");
             Scanner y = new Scanner(System.in);
             Point point = new Point(x.nextInt(),y.nextInt());
+            hexBoard.setCorner(point,hexBoard.getCorner(point));
+            player.buildSettlement(point);
+            System.out.println(siedlerGame.getPlayer().get(i) + "please pick a x coordinate for the start of your first road");
+            Scanner xroadStart = new Scanner(System.in);
+            System.out.println(siedlerGame.getPlayer().get(i) + "please pick a y coordinate for the start of your first road");
+            Scanner yroadStart = new Scanner(System.in);
+            Point roadpoint = new Point(xroadStart.nextInt(),yroadStart.nextInt());
+            System.out.println(siedlerGame.getPlayer().get(i) + "please pick a x coordinate for the finish of your first road");
+            Scanner xroadFinish = new Scanner(System.in);
+            System.out.println(siedlerGame.getPlayer().get(i) + "please pick a y coordinate for the finish of your first road");
+            Scanner yroadFinish = new Scanner(System.in);
+            Point roadend = new Point(xroadFinish.nextInt(),yroadFinish.nextInt());
+            hexBoard.setEdge(roadpoint,roadend,hexBoard.getEdge(roadpoint,roadend));
+            // TODO cant place the road for a player
         }
     }
     public static void main(String[] Args) {
