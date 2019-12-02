cc95fcb0c3065f49036260f9e785c8285a4705f2
Tobias Ritscher <tobirit@gmail.com>
2019-12-02 12:36:36 +0100

    changed global parameters to private

--- src/ch/zhaw/catan/Road.java
+++ src/ch/zhaw/catan/Road.java
@@ -3,17 +3,21 @@ package ch.zhaw.catan;
 import java.awt.Point;
 
 public class Road {
-    Point position;
-    Point startingAt;
-    Point endingAt;
+    private Point position;
+    private Point startingAt;
+    private Point endingAt;
 
     public Road(Point startingAt, Point endingAt) {
         this.startingAt = startingAt;
         this.endingAt = endingAt;
     }
 
-    public Point getStartingAt() { return startingAt; }
+    public Point getStartingAt() {
+        return startingAt;
+    }
 
-    public Point getEndingAt() { return endingAt; }
+    public Point getEndingAt() {
+        return endingAt;
+    }
 
 }
