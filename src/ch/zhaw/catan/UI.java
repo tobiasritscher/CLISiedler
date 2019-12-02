1ad885568d69a8ccc0cde6443f7a773c858f2855
Pascal Witzig <witzipa1@zhaw.ch>
2019-12-02 16:16:17 +0100

    Adapting labels for Map

--- src/ch/zhaw/catan/UI.java
+++ src/ch/zhaw/catan/UI.java
@@ -66,12 +66,32 @@ public class UI {
 
         board.setFields(board);
         SiedlerBoardTextView view = new SiedlerBoardTextView(board);
-        board.addField(new Point(2, 2), Config.Land.FOREST);
-        board.setCorner(new Point(3, 3), "RR");
-        board.setEdge(new Point(2, 0), new Point(3, 1), "r");
-        board.addFieldAnnotation(new Point(2, 2), new Point(3, 1), "AA");
+//        board.addField(new Point(2, 2), Config.Land.FOREST);
+//        board.setCorner(new Point(3, 3), "RR");
+//        board.setEdge(new Point(2, 0), new Point(3, 1), "r");
+//        board.addFieldAnnotation(new Point(2, 2), new Point(3, 1), "AA");
         Map<Point, ch.zhaw.hexboard.Label> lowerFieldLabel = new HashMap<>();
-        lowerFieldLabel.put(new Point(2, 2), new ch.zhaw.hexboard.Label('0', '9'));
+        lowerFieldLabel.put(new Point(5, 5), new ch.zhaw.hexboard.Label('0', '6'));
+        lowerFieldLabel.put(new Point(7, 5), new ch.zhaw.hexboard.Label('0', '3'));
+        lowerFieldLabel.put(new Point(9, 5), new ch.zhaw.hexboard.Label('0', '8'));
+        lowerFieldLabel.put(new Point(4, 8), new ch.zhaw.hexboard.Label('0', '2'));
+        lowerFieldLabel.put(new Point(6, 8), new ch.zhaw.hexboard.Label('0', '4'));
+        lowerFieldLabel.put(new Point(8, 8), new ch.zhaw.hexboard.Label('0', '5'));
+        lowerFieldLabel.put(new Point(10, 8), new ch.zhaw.hexboard.Label('1', '0'));
+        lowerFieldLabel.put(new Point(3, 11), new ch.zhaw.hexboard.Label('0', '5'));
+        lowerFieldLabel.put(new Point(5, 11), new ch.zhaw.hexboard.Label('0', '9'));
+        lowerFieldLabel.put(new Point(7, 11), new ch.zhaw.hexboard.Label('0', '7'));
+        lowerFieldLabel.put(new Point(9, 11), new ch.zhaw.hexboard.Label('0', '6'));
+        lowerFieldLabel.put(new Point(11, 11), new ch.zhaw.hexboard.Label('0', '9'));
+        lowerFieldLabel.put(new Point(4, 14), new ch.zhaw.hexboard.Label('1', '0'));
+        lowerFieldLabel.put(new Point(6, 14), new ch.zhaw.hexboard.Label('1', '1'));
+        lowerFieldLabel.put(new Point(8, 14), new ch.zhaw.hexboard.Label('0', '3'));
+        lowerFieldLabel.put(new Point(10, 14), new ch.zhaw.hexboard.Label('1', '2'));
+        lowerFieldLabel.put(new Point(5, 17), new ch.zhaw.hexboard.Label('0', '8'));
+        lowerFieldLabel.put(new Point(7, 17), new ch.zhaw.hexboard.Label('0', '4'));
+        lowerFieldLabel.put(new Point(9, 17), new ch.zhaw.hexboard.Label('1', '1'));
+
+
 
         for (Map.Entry<Point, Label> e : lowerFieldLabel.entrySet()) {
             view.setLowerFieldLabel(e.getKey(), e.getValue());
